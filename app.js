75ef7b328929424b9f15cf27a1fe1bf953462b61
maximusheaven <maximilianbrauner95@gmail.com>
2024-08-14 16:13:07 +0200

    basic setup and function for creating the grid

---
+++ app.js
@@ -0,0 +1,12 @@
+const gridContainer = document.querySelector(".grid-container");
+const square = document.createElement("div");
+
+function createGrid(size) {
+  for (i = 0; i < size ** 2; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.style.width = `${640 / size}px`;
+    square.style.height = `${640 / size}px`;
+    gridContainer.appendChild(square)
+  }
+}
