404aeea4c0de4af0aa0aa9ece04790c7c4a843b6
maximusheaven <maximilianbrauner95@gmail.com>
2024-08-15 11:01:32 +0200

    created functionality for custom grid size

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const gridContainer = document.querySelector(".grid-container");
 const square = document.createElement("div");
+const sizeButton = document.querySelector(".grid-size-button");
+
 
 function createGrid(size) {
   for (i = 0; i < size ** 2; i++) {
@@ -10,6 +12,25 @@ function createGrid(size) {
     gridContainer.appendChild(square);
     square.addEventListener("mouseover", () => {
       square.style.backgroundColor = "grey";
-    })
+    });
   }
 }
+
+createGrid(16);
+
+function clearGrid(){
+  gridContainer.innerHTML = ''
+}
+
+function gridSize() {
+  let size = Number(
+    prompt("What size should your grid be? Maximum size is 100")
+  );
+  if (size < 1 || size > 100) {
+    alert("The number must be between 1 and 100");
+    size = Number(prompt("Choose your grid size betweewn 1 and 100"));
+  }
+  clearGrid()
+  createGrid(size)
+}
+
