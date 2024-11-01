354e73d79c9a27273aa55460ce44621a74239a23
maximusheaven <maximilianbrauner95@gmail.com>
2024-11-01 09:02:04 +0100

    changed that you also draw a single square by clicking once

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ function createGrid(size) {
       if(mouseDown){
       square.style.backgroundColor = `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`;
     }});
+    square.addEventListener("click", () => {
+      square.style.backgroundColor = `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`;
+    });
   }
 }
 
